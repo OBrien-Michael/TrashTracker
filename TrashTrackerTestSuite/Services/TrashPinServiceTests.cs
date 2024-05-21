e0335947495069fd1bc7bb659f3a1b4c7ce9654a
Michael O'Brien <obrien.michael@outlook.ie>
2024-05-21 12:47:14 +0100

    Added TestSuite and refactored solution structure
    Tests for services and modelviews
    
    Refs #1

---
+++ TrashTrackerTestSuite/Services/TrashPinServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TrashTracker.Services;
+using Xunit;
+
+namespace TrashTrackerTestSuite.Services
+{
+	public class TrashPinServiceTests
+	{
+		[Fact]
+		public void GetAllTrashPins_ExpectListsToBeEqual()
+		{
+			// Arrange
+			var service = new TrashPinService();
+
+			// Act
+			var result = service.GetAllTrashPins();
+
+			// Assert
+			Assert.Equal(service.TrashPinList,result);
+		}
+	}
+}
---
+++ TrashTrackerTestSuite/ViewModel/MapPageViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TrashTracker.Services;
+using TrashTracker.ViewModel;
+using Xunit;
+
+namespace TrashTrackerTestSuite.ViewModel
+{
+	public class MapPageViewModelTests
+	{
+		[Fact]
+		public void LoadPins_ExpectPinsToBeLoaded()
+		{
+			// Arrange
+
+			TrashPinService trashPinService = new TrashPinService();
+
+			var viewModel = new MapPageViewModel(trashPinService);
+
+			// Act
+			viewModel.LoadPins();
+
+			// Assert
+			Assert.NotNull(viewModel.TrashPins);
+		}
+	}
+}
